a381f8a96090bebf54b29d0cdf31da33a7dace8c
0xGabi <gabrielpk.18@gmail.com>
2021-10-29 21:58:29 -0300

    DX: migrate to hardhat

--- test/helpers/assertExternalEvent.js
+++ test/helpers/assertExternalEvent.js
@@ -2,7 +2,7 @@ const sha3 = require('js-sha3').keccak_256
 
 module.exports = assertExternalEvent = (receipt, eventName, instances = 1) => {
   const logs = (receipt.receipt && receipt.receipt.rawLogs) || []
-  const events = logs.filter(l => {
+  const events = logs.filter((l) => {
     return l.topics[0] === '0x' + sha3(eventName)
   })
   assert.equal(
