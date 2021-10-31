becc13dd456e05d6b30ac3960a79b491e2251470
David Llop <931684+sembrestels@users.noreply.github.com>
2021-10-31 16:47:31 +0100

    Use msg.value directly in _collateralValueIsValid (fixes #13)

--- contracts/AugmentedBondingCurve.sol
+++ contracts/AugmentedBondingCurve.sol
@@ -347,7 +347,7 @@ contract AugmentedBondingCurve is EtherTokenConstant, IsContract, ApproveAndCall
         return _tokenManager.maxAccountTokens() == uint256(-1);
     }
 
-    function _collateralValueIsValid(address _buyer, address _collateral, uint256 _value, uint256 _msgValue, bool _noPreApproval)
+    function _collateralValueIsValid(address _buyer, address _collateral, uint256 _value, bool _noPreApproval)
         internal view returns (bool)
     {
         if (_value == 0) {
@@ -355,7 +355,7 @@ contract AugmentedBondingCurve is EtherTokenConstant, IsContract, ApproveAndCall
         }
 
         if (_collateral == ETH) {
-            return _msgValue == _value;
+            return msg.value == _value;
         }
 
         bool buyerAllowanceAvailable = !_noPreApproval &&
@@ -364,7 +364,7 @@ contract AugmentedBondingCurve is EtherTokenConstant, IsContract, ApproveAndCall
 
         bool fundsAlreadyDeposited = _noPreApproval && balanceOf(address(this), _collateral) >= _value;
 
-        return _msgValue == 0 && (buyerAllowanceAvailable || fundsAlreadyDeposited);
+        return msg.value == 0 && (buyerAllowanceAvailable || fundsAlreadyDeposited);
     }
 
     function _bondAmountIsValid(address _seller, uint256 _amount) internal view returns (bool) {
@@ -390,7 +390,7 @@ contract AugmentedBondingCurve is EtherTokenConstant, IsContract, ApproveAndCall
     {
         require(isOpen, ERROR_NOT_OPEN);
         require(_collateralIsWhitelisted(_collateral), ERROR_COLLATERAL_NOT_WHITELISTED);
-        require(_collateralValueIsValid(_buyer, _collateral, _depositAmount, msg.value, _noPreApproval), ERROR_INVALID_COLLATERAL_VALUE);
+        require(_collateralValueIsValid(_buyer, _collateral, _depositAmount, _noPreApproval), ERROR_INVALID_COLLATERAL_VALUE);
 
         // deduct fee
         uint256 fee = _depositAmount.mul(buyFeePct).div(PCT_BASE);
