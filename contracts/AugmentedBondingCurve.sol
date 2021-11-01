c855fb7934d41299241676c5a46d8fa6318af84b
David Llop <931684+sembrestels@users.noreply.github.com>
2021-11-01 17:37:55 +0100

    Simplify collateral managing roles to just one (fixes #8)

--- contracts/AugmentedBondingCurve.sol
+++ contracts/AugmentedBondingCurve.sol
@@ -20,9 +20,7 @@ contract AugmentedBondingCurve is EtherTokenConstant, IsContract, ApproveAndCall
     bytes32 public constant UPDATE_FORMULA_ROLE                        = keccak256("UPDATE_FORMULA_ROLE");
     bytes32 public constant UPDATE_BENEFICIARY_ROLE                    = keccak256("UPDATE_BENEFICIARY_ROLE");
     bytes32 public constant UPDATE_FEES_ROLE                           = keccak256("UPDATE_FEES_ROLE");
-    bytes32 public constant ADD_COLLATERAL_TOKEN_ROLE                  = keccak256("ADD_COLLATERAL_TOKEN_ROLE");
-    bytes32 public constant REMOVE_COLLATERAL_TOKEN_ROLE               = keccak256("REMOVE_COLLATERAL_TOKEN_ROLE");
-    bytes32 public constant UPDATE_COLLATERAL_TOKEN_ROLE               = keccak256("UPDATE_COLLATERAL_TOKEN_ROLE");
+    bytes32 public constant MANAGE_COLLATERAL_TOKEN_ROLE               = keccak256("MANAGE_COLLATERAL_TOKEN_ROLE");
     bytes32 public constant OPEN_TRADING_ROLE                          = keccak256("OPEN_TRADING_ROLE");
     bytes32 public constant MAKE_BUY_ORDER_ROLE                        = keccak256("MAKE_BUY_ORDER_ROLE");
     bytes32 public constant MAKE_SELL_ORDER_ROLE                       = keccak256("MAKE_SELL_ORDER_ROLE");
@@ -30,9 +28,7 @@ contract AugmentedBondingCurve is EtherTokenConstant, IsContract, ApproveAndCall
     bytes32 public constant UPDATE_FORMULA_ROLE                        = 0xbfb76d8d43f55efe58544ea32af187792a7bdb983850d8fed33478266eec3cbb;
     bytes32 public constant UPDATE_BENEFICIARY_ROLE                    = 0xf7ea2b80c7b6a2cab2c11d2290cb005c3748397358a25e17113658c83b732593;
     bytes32 public constant UPDATE_FEES_ROLE                           = 0x5f9be2932ed3a723f295a763be1804c7ebfd1a41c1348fb8bdf5be1c5cdca822;
-    bytes32 public constant ADD_COLLATERAL_TOKEN_ROLE                  = 0x217b79cb2bc7760defc88529853ef81ab33ae5bb315408ce9f5af09c8776662d;
-    bytes32 public constant REMOVE_COLLATERAL_TOKEN_ROLE               = 0x2044e56de223845e4be7d0a6f4e9a29b635547f16413a6d1327c58d9db438ee2;
-    bytes32 public constant UPDATE_COLLATERAL_TOKEN_ROLE               = 0xe0565c2c43e0d841e206bb36a37f12f22584b4652ccee6f9e0c071b697a2e13d;
+    bytes32 public constant MANAGE_COLLATERAL_TOKEN_ROLE               = 0xd9d296b0bc78eaab1039dfb623e942381a5402711b7fcec0bfb94004c18879f4;
     bytes32 public constant OPEN_TRADING_ROLE                          = 0x26ce034204208c0bbca4c8a793d17b99e546009b1dd31d3c1ef761f66372caf6;
     bytes32 public constant MAKE_BUY_ORDER_ROLE                        = 0x0dfea6908176d96adbee7026b3fe9fbdaccfc17bc443ddf14734fd27c3136179;
     bytes32 public constant MAKE_SELL_ORDER_ROLE                       = 0x52e3ace6a83e0c810920056ccc32fed5aa1e86287545113b03a52ab5c84e3f66;
@@ -209,7 +205,7 @@ contract AugmentedBondingCurve is EtherTokenConstant, IsContract, ApproveAndCall
      * @param _reserveRatio   The reserve ratio to be used for that collateral token [in PPM]
     */
     function addCollateralToken(address _collateral, uint256 _virtualSupply, uint256 _virtualBalance, uint32 _reserveRatio)
-        external auth(ADD_COLLATERAL_TOKEN_ROLE)
+        external auth(MANAGE_COLLATERAL_TOKEN_ROLE)
     {
         require(isContract(_collateral) || _collateral == ETH, ERROR_INVALID_COLLATERAL);
         require(!_collateralIsWhitelisted(_collateral),        ERROR_COLLATERAL_ALREADY_WHITELISTED);
@@ -222,7 +218,7 @@ contract AugmentedBondingCurve is EtherTokenConstant, IsContract, ApproveAndCall
       * @notice Remove `_collateral.symbol(): string` as a whitelisted collateral token
       * @param _collateral The address of the collateral token to be un-whitelisted
     */
-    function removeCollateralToken(address _collateral) external auth(REMOVE_COLLATERAL_TOKEN_ROLE) {
+    function removeCollateralToken(address _collateral) external auth(MANAGE_COLLATERAL_TOKEN_ROLE) {
         require(_collateralIsWhitelisted(_collateral), ERROR_COLLATERAL_NOT_WHITELISTED);
 
         _removeCollateralToken(_collateral);
@@ -236,7 +232,7 @@ contract AugmentedBondingCurve is EtherTokenConstant, IsContract, ApproveAndCall
      * @param _reserveRatio   The new reserve ratio to be used for that collateral token [in PPM]
     */
     function updateCollateralToken(address _collateral, uint256 _virtualSupply, uint256 _virtualBalance, uint32 _reserveRatio)
-        external auth(UPDATE_COLLATERAL_TOKEN_ROLE)
+        external auth(MANAGE_COLLATERAL_TOKEN_ROLE)
     {
         require(_collateralIsWhitelisted(_collateral), ERROR_COLLATERAL_NOT_WHITELISTED);
         require(_reserveRatioIsValid(_reserveRatio),   ERROR_INVALID_RESERVE_RATIO);
